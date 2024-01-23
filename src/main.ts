5d799fc4d05b83fa28d651109b69342cabe8b1b5
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-23 18:03:08 +0100

    update links

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import '@picocss/pico';
 import './css/style.scss';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.mount('#app')
+app.mount('#app');
