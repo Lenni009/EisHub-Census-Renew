9626a2b3215f30b050fb299486e4709bf7e03118
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-22 20:08:17 +0100

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Eisvana-Census-Renew/',
+//   base: '/Eisvana-Census-Renew/',
   plugins: [vue()],
   resolve: {
     alias: {
