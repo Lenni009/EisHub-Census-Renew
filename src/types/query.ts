d579dd41e9c7fbb720d09b64cfee2940eeea2ca6
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-24 00:24:02 +0100

    expand query types

--- src/types/query.ts
+++ src/types/query.ts
@@ -1,16 +1,26 @@
 interface RawCensusEntry {
   Name: string;
   CensusPlayer: string;
+  CensusDiscord: string;
+  CensusFriend: string;
+  CensusReddit: string;
+  Mode: string;
+  Platform: string;
+  System: string;
 }
 
 // change string to array for later code to consume
 export interface CensusEntry extends RawCensusEntry {
   CensusRenewal: string[];
+  CensusArrival: Date;
+  GameRelease: string;
 }
 
 // returned from query
 interface QueryCensusEntry extends RawCensusEntry {
   CensusRenewal: string;
+  CensusArrival: string;
+  'Game release': string;
 }
 
 // API query returns an array of these
