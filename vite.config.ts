516ac6c30d67249df06fff7b17afc77ecd33e06e
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 21:54:26 +0100

    more HTML entry points

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//   base: '/Eisvana-Census-Renew/',
+  //   base: '/Eisvana-Census-Renew/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: ['index.html', 'form.html', 'renew.html'],
     },
   },
 });
