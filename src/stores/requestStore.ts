37a72374a9a80c317167856ac570cbfc04b79beb
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 23:38:51 +0100

    add store for requests

---
+++ src/stores/requestStore.ts
@@ -0,0 +1,18 @@
+import type { CensusEntry } from '@/types/query';
+import { defineStore } from 'pinia';
+
+interface RequestStore {
+  requestSent: boolean;
+  requestSucceeded: boolean;
+  requestFailed: boolean;
+  censusData: CensusEntry[];
+}
+
+export const useRequestStore = defineStore('requests', {
+  state: (): RequestStore => ({
+    requestSent: false,
+    requestSucceeded: false,
+    requestFailed: false,
+    censusData: [],
+  }),
+});
