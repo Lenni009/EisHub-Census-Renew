9f1cec64257db757b1ec5548454499f6880f07fa
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 23:38:20 +0100

    comment types

--- src/types/query.ts
+++ src/types/query.ts
@@ -3,14 +3,17 @@ interface RawCensusEntry {
   CensusPlayer: string;
 }
 
+// change string to array for later code to consume
 export interface CensusEntry extends RawCensusEntry {
   CensusRenewal: string[];
 }
 
+// returned from query
 interface QueryCensusEntry extends RawCensusEntry {
   CensusRenewal: string;
 }
 
+// API query returns an array of these
 export interface QueryEntry {
   title: QueryCensusEntry;
 }
