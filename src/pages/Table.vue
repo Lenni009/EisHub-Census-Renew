bc007d0531948ba83c0d31f40ac5d9ab56000929
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-24 18:54:25 +0100

    remove unneeded code

--- src/pages/Table.vue
+++ src/pages/Table.vue
@@ -44,58 +44,6 @@ const filteredCensusCount = computed(() => filteredEntries.value.length);
         :entry
       />
     </div>
-
-    <!--
-    <div class="census-table">
-      <div class="heading">Player Name</div>
-      <div class="heading">Social Media Link</div>
-      <div class="heading">Discord Name</div>
-      <div class="heading">Friend Code</div>
-      <div class="heading">Home Base</div>
-      <div class="heading">System</div>
-      <div class="heading">Platform</div>
-      <div class="heading">Game Mode</div>
-      <div class="heading">Date of Arrival</div>
-      <div class="heading">Renewed</div>
-      <div class="heading">Release</div>
-      <div class="heading">Renew Entry</div>
-      <div class="heading">Update Base</div>
-      <template v-for="entry in filteredEntries">
-        <div>{{ entry.CensusPlayer }}</div>
-        <div>
-          <a
-            :href="parseUserLink(entry.CensusReddit ?? '').link"
-            rel="noopener noreferrer"
-            target="_blank"
-            >{{ parseUserLink(entry.CensusReddit ?? '').text }}</a
-          >
-        </div>
-        <div>{{ entry.CensusDiscord }}</div>
-        <div>{{ entry.CensusFriend }}</div>
-        <div>
-          <a
-            :href="`${wikiLink}${entry.Name}`"
-            rel="noopener noreferrer"
-            target="_blank"
-            >{{ entry.Name }}</a
-          >
-        </div>
-        <div>{{ entry.System }}</div>
-        <div>{{ entry.Platform }}</div>
-        <div>{{ entry.Mode }}</div>
-        <div>{{ entry.CensusArrival.toLocaleDateString() }}</div>
-        <div>{{ entry.CensusRenewal.join(', ') }}</div>
-        <div>{{ entry.GameRelease }}</div>
-        <div><button>Renew</button></div>
-        <div>
-          <a
-            href="./form"
-            role="button"
-            >Update</a
-          >
-        </div>
-      </template>
-    </div>-->
   </div>
 </template>
 
@@ -120,25 +68,9 @@ const filteredCensusCount = computed(() => filteredEntries.value.length);
   }
 }
 
-.census-table {
-  display: grid;
-  grid-template-columns: repeat(13, auto);
-  gap: 0.5rem;
-  overflow: auto;
-
-  & * {
-    border-block-end: 1px solid var(--pico-table-border-color);
-  }
-
-  .heading {
-    font-weight: bold;
-  }
-}
-
 .census-items {
   display: flex;
   flex-wrap: wrap;
   gap: 1rem;
-
 }
 </style>
