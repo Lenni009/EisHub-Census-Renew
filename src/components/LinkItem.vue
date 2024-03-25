ea85d1d49534e6e449b5f0a0169f915ed9b00f84
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-25 20:46:46 +0100

    fix wiki link

--- src/components/LinkItem.vue
+++ src/components/LinkItem.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { parseUserLink } from '../helpers/wikitextParser';
+import { wikiLink } from '@/variables/wikiLink';
 
 const props = defineProps<{
   wikitext: string;
   text?: string;
 }>();
 
-const linkObj = computed(() => parseUserLink(props.wikitext));
+const linkObj = computed(() =>
+  props.wikitext.startsWith('[')
+    ? parseUserLink(props.wikitext)
+    : { text: props.wikitext, link: `${wikiLink}${props.wikitext}` }
+);
 </script>
 
 <template>
