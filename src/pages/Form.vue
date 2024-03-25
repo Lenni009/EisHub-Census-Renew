ccc125d6450514ca551f5233cb76e4cd9e2890f6
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-25 17:50:18 +0100

    add POC API integration

--- src/pages/Form.vue
+++ src/pages/Form.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { decodePlayerName } from '@/helpers/nameTranscode';
+import { onMounted, ref } from 'vue';
 
 const searchParamString = window.location.search;
 const searchParams = new URLSearchParams(searchParamString);
@@ -12,8 +13,21 @@ const playerDataString = sessionStorage.getItem('update');
 const playerData = JSON.parse(playerDataString ?? '{}');
 
 console.log(updatePlayerId, playerData);
+
+const apiCall =
+  'https://nomanssky.fandom.com/api.php?action=parse&format=json&origin=*&page=Eisvana%20Research%20Outpost&prop=wikitext&section=3&contentmodel=wikitext';
+
+const wikitext = ref('');
+
+onMounted(async () => {
+  const res = await fetch(apiCall);
+  const { parse } = await res.json();
+  wikitext.value = parse.wikitext['*'];
+});
 </script>
 
 <template>
   <div>Hello world!</div>
+
+  <textarea v-model="wikitext"></textarea>
 </template>
