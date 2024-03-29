516c101708279cc2eb4c5c018a2fa565d82f0535
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-29 18:59:15 +0100

    store webhook data in variables

---
+++ src/variables/env.ts
@@ -0,0 +1,2 @@
+export const renewWebhook: string = atob(import.meta.env.VITE_DISCORD_RENEW_WEBHOOK ?? '');
+export const formWebhook: string = atob(import.meta.env.VITE_DISCORD_FORM_WEBHOOK ?? '');
