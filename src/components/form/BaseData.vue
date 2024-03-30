4c75825bd8839f7775b9d6f8dcc866c6a454cbdc
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-30 20:41:32 +0100

    form UI

---
+++ src/components/form/BaseData.vue
@@ -0,0 +1,219 @@
+<script setup lang="ts">
+import { useWikiPageDataStore } from '@/stores/wikiPageDataStore';
+import { storeToRefs } from 'pinia';
+import MultipleChoice from './MultipleChoice.vue';
+import GlyphInput from './GlyphInput.vue';
+import { watchEffect, reactive } from 'vue';
+import { regionArray } from '@/variables/regions';
+
+const wikiPageData = useWikiPageDataStore();
+const {
+  platform,
+  mode,
+  baseName,
+  region,
+  glyphs,
+  system,
+  planet,
+  moon,
+  axes,
+  arena,
+  terminal,
+  landingpad,
+  geobay,
+  farm,
+  racetrack,
+  type,
+  layout,
+  features,
+  addInfo,
+  image,
+  gallery,
+} = storeToRefs(wikiPageData);
+
+watchEffect(() => (region.value = regionArray.find((item) => item[0] === glyphs.value.slice(4))?.[1] ?? ''));
+
+const platforms: Record<string, string> = {
+  PC: 'PC',
+  PS: 'PlayStation',
+  XB: 'Xbox',
+  NS: 'Switch',
+};
+
+const modes: Record<string, string> = {
+  Normal: 'Normal',
+  Relaxed: 'Relaxed',
+  Survival: 'Survival',
+  Permadeath: 'Permadeath',
+  Creative: 'Creative',
+  Custom: 'Custom',
+};
+
+const featureList = reactive([
+  {
+    label: 'Landing Pad',
+    value: landingpad,
+  },
+  {
+    label: 'Trade Terminal',
+    value: terminal,
+  },
+  {
+    label: 'Geobay',
+    value: geobay,
+  },
+  {
+    label: 'Farm',
+    value: farm,
+  },
+  {
+    label: 'Racetrack',
+    value: racetrack,
+  },
+  {
+    label: 'Arena',
+    value: arena,
+  },
+]);
+
+function uploadMainFile(e: Event) {
+  const { target } = e;
+  if (!(target instanceof HTMLInputElement)) return;
+  image.value = target.files?.[0] ?? null; // target.files could be null, and since optional chaining returns undefined, we have to specify null as fallback ourselves. The image variable can also take null as value.
+}
+
+function uploadGallery(e: Event) {
+  const { target } = e;
+  if (!(target instanceof HTMLInputElement)) return;
+  const fileArray = Array.from(target.files ?? []);
+  gallery.value.push(...fileArray);
+}
+</script>
+
+<template>
+  <article>
+    <p class="question">What is the name of your base?</p>
+    <input
+      v-model="baseName"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">What is the name of the system where your base is?</p>
+    <p class="subtitle">The name of the system can be seen by looking at the galaxy map in space.</p>
+    <input
+      v-model="system"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">
+      What is the name of the planet on which your base is or that is orbited by the moon on which your base is?
+    </p>
+    <input
+      v-model="planet"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">If your base is on a moon (else leave empty): What is the name of the moon?</p>
+    <input
+      v-model="moon"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">What are the planetary coordinates of your base?</p>
+    <input
+      v-model="axes"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">What are the glyphs?</p>
+    <p class="subtitle">Glyphs can be found in photo mode</p>
+    <GlyphInput v-model="glyphs" />
+  </article>
+  <article>
+    <p class="question">Base Features: Check all that apply</p>
+    <div class="checkboxes">
+      <label v-for="feature in featureList">
+        <input
+          v-model="feature.value"
+          type="checkbox"
+        />
+        <span>{{ feature.label }}</span>
+      </label>
+    </div>
+  </article>
+  <article>
+    <p class="question">What type of base do you have? What is its purpose?</p>
+    <p class="subtitle">Some examples are Artistic, Embassy, Farm, Industrial, Memorial and Residential.</p>
+    <input
+      v-model="type"
+      type="text"
+    />
+  </article>
+  <article>
+    <p class="question">On which platform was the base built?</p>
+    <MultipleChoice
+      v-model="platform"
+      :items="platforms"
+      name="platform"
+    />
+  </article>
+  <article>
+    <p class="question">On which Game Mode was the base built?</p>
+    <MultipleChoice
+      v-model="mode"
+      :items="modes"
+      name="mode"
+    />
+  </article>
+  <article>
+    <p class="question">Layout</p>
+    <p class="subtitle">What does the base look like?</p>
+    <textarea v-model="layout"></textarea>
+  </article>
+  <article>
+    <p class="question">Features</p>
+    <p class="subtitle">List the basic features, such as crops, biodomes, landing pads, exocraft bays, etc.</p>
+    <textarea v-model="features"></textarea>
+  </article>
+  <article>
+    <p class="question">Additional Information</p>
+    <p class="subtitle">Any nearby resources, tourist traps, other bases</p>
+    <textarea v-model="addInfo"></textarea>
+  </article>
+  <article>
+    <p class="question">Main picture</p>
+    <p class="subtitle">Picture clearly showing the entire base</p>
+    <input
+      type="file"
+      @change="uploadMainFile"
+    />
+  </article>
+  <article>
+    <p class="question">Gallery</p>
+    <p class="subtitle">Other pictures</p>
+    <input
+      type="file"
+      multiple
+      @change="uploadGallery"
+    />
+  </article>
+</template>
+
+<style scoped lang="scss">
+.checkboxes {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+@media (width < 576px) {
+  .checkboxes {
+    grid-template-columns: 1fr;
+    gap: 0.5rem;
+  }
+}
+</style>
---
+++ src/components/form/PlayerData.vue
@@ -0,0 +1,149 @@
+<script setup lang="ts">
+import { computed, watchEffect } from 'vue';
+import {
+  discordValidation,
+  validateDiscord,
+  isValidHttpUrl,
+  validateReddit,
+  validatePlayerName,
+  validateFriendCode,
+} from '@/helpers/formValidation';
+import { storeToRefs } from 'pinia';
+import { timezoneOffset } from '@/variables/dateTime';
+import { useWikiPageDataStore } from '@/stores/wikiPageDataStore';
+
+const wikiPageData = useWikiPageDataStore();
+const {
+  discord,
+  reddit,
+  social,
+  player,
+  friend,
+  arrival,
+  shareTimezone,
+  activeTime,
+} = storeToRefs(wikiPageData);
+
+const isDiscordValid = computed(() => validateDiscord(discord.value));
+const isRedditValid = computed(() => validateReddit(reddit.value));
+const isSocialValid = computed(() => isValidHttpUrl(social.value));
+const isNameValid = computed(() => validatePlayerName(player.value));
+const isFriendValid = computed(() => validateFriendCode(friend.value));
+
+watchEffect(() => (friend.value = friend.value.toUpperCase()));
+</script>
+
+<template>
+  <article>
+    <p class="question">What is your Discord name?</p>
+    <p class="subtitle">Please enter your username, not your display name.</p>
+    <input
+      v-model="discord"
+      :aria-invalid="!isDiscordValid || undefined"
+      :pattern="discordValidation"
+      type="text"
+    />
+    <p
+      v-if="!isDiscordValid"
+      class="error"
+    >
+      Use your username, not your display name
+    </p>
+  </article>
+  <article>
+    <p class="question">What is your Reddit name?</p>
+    <input
+      v-model="reddit"
+      :aria-invalid="!isRedditValid || undefined"
+      type="text"
+    />
+    <p
+      v-if="!isRedditValid"
+      class="error"
+    >
+      Please use your Reddit username without the u/
+    </p>
+  </article>
+  <article>
+    <p class="question">Social media link if you don't have Reddit</p>
+    <p class="subtitle">
+      If you don't have Reddit, do you have a different social media account that you'd like to have on the census
+      (Facebook, Instagram, Wiki, etc.)? Please put the full link here.
+    </p>
+    <input
+      v-model="social"
+      :aria-invalid="!isSocialValid || undefined"
+      type="text"
+    />
+    <p
+      v-if="!isSocialValid"
+      class="error"
+    >
+      Please give a link to that profile.
+    </p>
+  </article>
+  <article>
+    <p class="question">What is your ingame name?</p>
+    <p class="subtitle">Found on the top left in your inventory/pause menu. Exclude any game titles.</p>
+    <input
+      v-model="player"
+      :aria-invalid="!isNameValid || undefined"
+      type="text"
+    />
+    <p
+      v-if="!isNameValid"
+      class="error"
+    >
+      Please enter your ingame name, not your platform
+    </p>
+  </article>
+  <article>
+    <p class="question">No Man's Sky Friend Code</p>
+    <p class="subtitle">Please enter your friend code in the following format: "XXXX-XXXX-XXXXX" including dashes.</p>
+    <input
+      v-model="friend"
+      :aria-invalid="!isFriendValid || undefined"
+      type="text"
+    />
+    <p
+      v-if="!isFriendValid"
+      class="error"
+    >
+      Format must be as follows: XXXX-XXXX-XXXXX
+    </p>
+  </article>
+  <article>
+    <p class="question">Date of Arrival</p>
+    <p class="subtitle">When did you arrive in Eisvana space?</p>
+    <input
+      v-model="arrival"
+      type="date"
+    />
+  </article>
+  <article>
+    <p class="question">[OPTIONAL] Timezone</p>
+    <p class="subtitle">
+      Detected timezone: <span class="text-bold">{{ timezoneOffset }}</span>
+      <br />
+      This will be used to plan future events accordingly. This information will remain private and will only be shared
+      with Eisvana Leadership.
+    </p>
+    <label>
+      <span>Share timezone ({{ timezoneOffset }})</span>
+      <input
+        v-model="shareTimezone"
+        type="checkbox"
+      />
+    </label>
+  </article>
+  <article>
+    <p class="question">[OPTIONAL] In this time zone, when do you play No Man's Sky the most?</p>
+    <p class="subtitle">
+      I. e. mornings, afternoon, evenings, late night, 9 AM to 12 PM, etc. This is used in order to better plan events.
+    </p>
+    <input
+      v-model="activeTime"
+      type="text"
+    />
+  </article>
+</template>
