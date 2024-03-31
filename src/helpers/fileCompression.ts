488a888f47a25d92029b00919d55baac8854d43e
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-31 18:35:26 +0200

    set proper file extension

--- src/helpers/fileCompression.ts
+++ src/helpers/fileCompression.ts
@@ -1,12 +1,10 @@
 import { imageTypes, compressImage } from 'simple-image-compressor';
-import { escapeFileName } from './fileNameEscape';
 import { maxFileSize } from '@/variables/fileSize';
 
-export async function compressFile(inputFile: File, quality: number = 1): Promise<File> {
-  const sanitisedFileName = escapeFileName(inputFile.name);
-  const file = new File([inputFile], sanitisedFileName, { type: inputFile.type });
+export async function compressFile(file: File, quality: number = 1): Promise<File> {
   if (file.size < maxFileSize) return file; // if below 25 MB, don't do anything
-  const type = imageTypes.WEBP;
+  const type = imageTypes.JPEG;
+  const newFileExtension = type.split('/').at(-1);
   const res = await compressImage(file, {
     quality,
     type,
@@ -14,6 +12,6 @@ export async function compressFile(inputFile: File, quality: number = 1): Promis
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
   if (res.size > maxFileSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
-  const newFileName = `${fileName}-min.${type}`;
+  const newFileName = `${fileName}-min.${newFileExtension}`;
   return new File([res], newFileName, { type });
 }
