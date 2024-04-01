39a0f3b61d1e5ffec0879df51523c99d054ce8d7
Lenni009 <guentherhanslphdyt@gmail.com>
2024-04-01 19:16:55 +0200

    fix logic flaw

--- src/variables/formMode.ts
+++ src/variables/formMode.ts
@@ -3,4 +3,4 @@ import { isNewPage, isUpdating } from '@/helpers/censusForm';
 export const isUpdatingPage = isUpdating();
 export const isMakingNewPage = isNewPage();
 
-export const isNewCitizen = isUpdatingPage || isMakingNewPage;
+export const isNewCitizen = !isUpdatingPage && !isMakingNewPage;
