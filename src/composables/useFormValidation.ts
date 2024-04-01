77bd3772c413d830c761efd01acbab388d3d124a
Lenni009 <guentherhanslphdyt@gmail.com>
2024-04-01 14:32:10 +0200

    fix boolean validation

--- src/composables/useFormValidation.ts
+++ src/composables/useFormValidation.ts
@@ -2,7 +2,7 @@ import { useWikiPageDataStore } from '@/stores/wikiPageDataStore';
 import type { BaseData, PlayerData, ImageData, OptionalProps } from '@/types/pageData';
 import { optionalProperties } from '@/variables/formValidation';
 import { storeToRefs } from 'pinia';
-import { computed, ref, type Ref } from 'vue';
+import { computed, toRefs, type Ref, reactive } from 'vue';
 
 interface PlayerDataValidation {
   discord: boolean;
@@ -10,11 +10,13 @@ interface PlayerDataValidation {
   social: boolean;
   name: boolean;
   friend: boolean;
+  wikiName: boolean;
 }
 
 export function useFormValidation() {
   const wikiPageDataStore = useWikiPageDataStore();
   const {
+    validation,
     playerData,
     imageData,
     baseData,
@@ -26,23 +28,23 @@ export function useFormValidation() {
     isFriendValid,
     isAxesValid,
   } = storeToRefs(wikiPageDataStore);
+  const { wikiUserExists } = toRefs(validation.value);
 
   // this has to be declared this way instead of ref<PlayerDataValidation>({}) because otherwise the function wouldn't take it...
-  const playerDataValidation: Ref<PlayerDataValidation> = ref({
+  const playerDataValidation: PlayerDataValidation = reactive({
     discord: isDiscordValid,
     reddit: isRedditValid,
     social: isSocialValid,
     name: isNameValid,
     friend: isFriendValid,
+    wikiName: wikiUserExists,
   });
 
   const { isDataValid: isPlayerDataValid, missingProps: missingPlayerProps } = useDataValidation(playerData);
   const { isDataValid: isBaseDataValid, missingProps: missingBaseProps } = useDataValidation(baseData);
   const { isDataValid: isImageDataValid, missingProps: missingImageProps } = useDataValidation(imageData);
-  const { isDataValid: isPlayerValidationDataValid, missingProps: missingPlayerValidationProps } = useDataValidation(
-    playerDataValidation,
-    true
-  );
+  const { isDataValid: isPlayerValidationDataValid, missingProps: missingPlayerValidationProps } =
+    useValidationData(playerDataValidation);
 
   const missingPageOneProps = computed(() => [...missingPlayerProps.value, ...missingPlayerValidationProps.value]);
   const isPageOneValid = computed(() => isPlayerDataValid.value && isPlayerValidationDataValid.value);
@@ -70,16 +72,22 @@ export function useFormValidation() {
   };
 }
 
-function useDataValidation(
-  storeObj: Ref<ImageData> | Ref<PlayerData> | Ref<BaseData> | Ref<PlayerDataValidation>,
-  checkBoolean: boolean = false
-) {
+function useValidationData(validationObj: PlayerDataValidation) {
+  const entries = computed(() => Object.entries(validationObj));
+  const invalidValues = computed(() => entries.value.filter((item) => !item[1]));
+  const missingProps = computed(() => invalidValues.value.map((item) => item[0]));
+  const isDataValid = computed(() => !missingProps.value.length);
+
+  return { isDataValid, missingProps };
+}
+
+function useDataValidation(storeObj: Ref<ImageData> | Ref<PlayerData> | Ref<BaseData> | Ref<PlayerDataValidation>) {
   const entries = computed(() => Object.entries(storeObj.value));
   const filteredEntries = computed(() =>
     entries.value.filter((item) => isFormProperty(item[0]) && !optionalProperties.includes(item[0]))
   );
   const invalidValues = computed(() =>
-    filteredEntries.value.filter((item) => !item[1] && (checkBoolean || typeof item[1] !== 'boolean'))
+    filteredEntries.value.filter((item) => !item[1] && typeof item[1] !== 'boolean')
   );
   const missingProps = computed(() => invalidValues.value.map((item) => item[0]));
   const isDataValid = computed(() => !invalidValues.value.length); // return true if everything is good, else return false
