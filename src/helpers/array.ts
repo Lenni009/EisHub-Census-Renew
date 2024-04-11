ab3374172a273f4381dd0be93f4ebf614be408e1
Lenni009 <guentherhanslphdyt@gmail.com>
2024-04-11 19:48:27 +0200

    add comment

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -7,6 +7,7 @@ export function removeDuplicates<T>(arr: T[]): T[] {
 }
 
 export function paginateFiles(arr: File[]): File[][] {
+  // sort by filesize; descending (largest first, smallest last)
   const sortedFiles = arr.toSorted((a, b) => b.size - a.size);
 
   const chunks: File[][] = [];
@@ -15,18 +16,18 @@ export function paginateFiles(arr: File[]): File[][] {
 
   for (const file of sortedFiles) {
     if (currentChunk.length < maxFilesPerMessage && currentSize + file.size < maxMessageSize) {
-      // Add the file to the current chunk
+      // add the file to the current chunk
       currentChunk.push(file);
       currentSize += file.size;
     } else {
-      // Start a new chunk
+      // start a new chunk
       chunks.push(currentChunk);
       currentChunk = [file];
       currentSize = file.size;
     }
   }
 
-  // Add the last chunk if it's not empty
+  // add the last chunk if it's not empty
   if (currentChunk.length) chunks.push(currentChunk);
 
   return chunks;
